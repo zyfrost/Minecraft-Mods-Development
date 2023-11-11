4b03e3e61ca00e815d66461e039bfd14f11212e6
tanmay <murkutetanmay@gmail.com>
2023-11-11 23:53:04 +0530

    add boiler plate files to fabric mods

---
+++ src/main/java/net/tanmay/tutorialmod/TutorialModClient.java
@@ -0,0 +1,13 @@
+package net.tanmay.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
