823359cfde9b70488d1eab6e514c42d1b597bc7b
tanmay <murkutetanmay@gmail.com>
2023-11-12 00:50:15 +0530

    add Ruby and Raw_Ruby items & add Creative tab

--- src/main/java/net/tanmay/tutorialmod/TutorialMod.java
+++ src/main/java/net/tanmay/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.tanmay.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tanmay.tutorialmod.item.ModItemGroups;
+import net.tanmay.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,9 +16,9 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		LOGGER.info("Tanmay Murkute");
+
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+
 	}
 }
---
+++ src/main/java/net/tanmay/tutorialmod/item/ModItemGroups.java
@@ -0,0 +1,31 @@
+package net.tanmay.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.tanmay.tutorialmod.TutorialMod;
+
+public class ModItemGroups {
+public static final ItemGroup RUBY_GROUP = Registry.register(Registries.ITEM_GROUP,
+        new Identifier(TutorialMod.MOD_ID,"ruby"),
+        FabricItemGroup.builder().displayName(Text.translatable("itemgroup.ruby"))
+            .icon(()->new ItemStack(ModItems.RUBY)).entries((displayContext, entries) ->
+                {
+                    entries.add(ModItems.RUBY);
+                    entries.add(ModItems.RAW_RUBY);
+                    entries.add(Items.DIAMOND_HOE);
+                    entries.add(Items.DIAMOND);
+
+                }).build());
+
+
+
+    public static void registerItemGroups(){
+        TutorialMod.LOGGER.info("Registering Item Groups for :" + TutorialMod.MOD_ID);
+    }
+}
