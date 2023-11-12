6c85c78348a90674e29419037a2bed425a54cb79
tanmay <murkutetanmay@gmail.com>
2023-11-13 01:11:09 +0530

    add MetalDetector item (advanced item )

--- src/main/java/net/tanmay/tutorialmod/item/ModItems.java
+++ src/main/java/net/tanmay/tutorialmod/item/ModItems.java
@@ -11,10 +11,13 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.tanmay.tutorialmod.TutorialMod;
+import net.tanmay.tutorialmod.item.custom.MetalDetectorItem;
 
 public class ModItems {
     public static final Item RUBY =registerItem("ruby",new Item(new FabricItemSettings()));
     public static final Item RAW_RUBY =registerItem("raw_ruby",new Item(new FabricItemSettings()));
+    public static final Item METAL_DETECTOR =registerItem("metal_detector",
+            new MetalDetectorItem(new FabricItemSettings().maxDamage(64)));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries){
         entries.add(RUBY);
